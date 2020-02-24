12954b4c2f284bf91ffb0fb79234afe1f1cec5cf
Robbie Nesmith <robbie@newbo.co>
2020-02-24 15:03:20 -0600

    add db interaction

--- dumbserver.py
+++ dumbserver.py
@@ -1,10 +1,26 @@
 from flask import Flask
 
-app = Flask(__name__)
+import os
+import psycopg2
 
-def setup_db():
-	pass
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+app = Flask(__name__)
 
 @app.route("/")
 def index():
   return "Index page"
+
+@app.route("/list")
+def list_items():
+  conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+  cur = conn.cursor()
+	cur.execute("SELECT * FROM test")
+  results = cur.fetchall()
+  cur.close()
+  conn.close()
+  out = ""
+	for res in results:
+		out += " ".join(res)
+		out += "\n"
+  return out
